fca29d96697a45a7d96d6ca8b060f5497f22aa94
jiaqianglim <jqlimjiaqiang@gmail.com>
2022-08-12 09:15:13 +0800

    first commit

---
+++ src/main/java/nus/ssfa/SsfaApplication.java
@@ -0,0 +1,13 @@
+package nus.ssfa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SsfaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SsfaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/nus/ssfa/SsfaApplicationTests.java
@@ -0,0 +1,13 @@
+package nus.ssfa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SsfaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
